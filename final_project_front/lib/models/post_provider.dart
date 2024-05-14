14ccc7fa1f0ea265345f44dd9cb17bc702948062
XiaoGuai <jses0922737039@gmail.com>
2024-05-15 00:41:21 +0800

    chore: Refactor PostProvider to track loading state and handle errors in AutoLoginHandler

--- final_project_front/lib/models/post_provider.dart
+++ final_project_front/lib/models/post_provider.dart
@@ -1,11 +1,11 @@
-cimport 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:socket_io_client/socket_io_client.dart' as io;
 import 'lost_thing_and_Url.dart';
 
 class PostProvider with ChangeNotifier {
   List<LostThing> posts = [];
   late io.Socket socket;
-  bool isLoading = true;  // Add this to track loading state
+  bool isLoading = true; // Add this to track loading state
 
   PostProvider() {
     connectAndListen();
@@ -27,7 +27,7 @@ class PostProvider with ChangeNotifier {
       print('Received posts data: $data');
       try {
         posts = (data as List).map((item) => LostThing.fromMap(item)).toList();
-        isLoading = false;  // Update loading state
+        isLoading = false; // Update loading state
         notifyListeners();
       } catch (e) {
         print('Error parsing posts data: $e');
@@ -40,7 +40,7 @@ class PostProvider with ChangeNotifier {
 
     socket.onError((data) {
       print('Error: $data');
-      isLoading = false;  // Ensure to update on error too
+      isLoading = false; // Ensure to update on error too
       notifyListeners();
     });
   }
