ea9d8249bf5ef3d074b6e5bb5b81d84bf84152b1
box159 <aa94022728@gmail.com>
2024-05-15 00:44:24 +0800

    chore: Update PostProvider to track loading state and handle error in AutoLoginHandler

--- final_project_front/lib/models/post_provider.dart
+++ final_project_front/lib/models/post_provider.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:socket_io_client/socket_io_client.dart' as io;
-import '../models/lost_thing.dart';
+import 'lost_thing_and_Url.dart';
 
 class PostProvider with ChangeNotifier {
   List<LostThing> posts = [];
   late io.Socket socket;
-  VoidCallback? onPostsLoaded;
+  bool isLoading = true;  // Add this to track loading state
 
   PostProvider() {
     connectAndListen();
@@ -23,33 +23,25 @@ class PostProvider with ChangeNotifier {
       socket.emit('get_posts', 'Client is connected!');
     });
 
-    socket.onConnectError((data) {
-      print('Connection Error: $data');
-    });
-
-    socket.onConnectTimeout((data) {
-      print('Connection Timeout: $data');
-    });
-
-    socket.onError((data) {
-      print('Error: $data');
+    socket.on('posts_data', (data) {
+      print('Received posts data: $data');
+      try {
+        posts = (data as List).map((item) => LostThing.fromMap(item)).toList();
+        isLoading = false;  // Update loading state
+        notifyListeners();
+      } catch (e) {
+        print('Error parsing posts data: $e');
+      }
     });
 
     socket.onDisconnect((_) {
       print('Disconnected from WebSocket Server');
     });
 
-    socket.on('posts_data', (data) {
-      print('Received posts data: $data');
-      try {
-        posts = (data as List).map((item) => LostThing.fromMap(item)).toList();
-        notifyListeners();
-        if (onPostsLoaded != null) {
-          onPostsLoaded!(); // Call the callback when posts data is loaded
-        }
-      } catch (e) {
-        print('Error parsing posts data: $e');
-      }
+    socket.onError((data) {
+      print('Error: $data');
+      isLoading = false;  // Ensure to update on error too
+      notifyListeners();
     });
   }
 
