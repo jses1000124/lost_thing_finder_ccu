a0e55f75e3162a82649362d0f4724747edbe9564
box159 <aa94022728@gmail.com>
2024-05-14 21:58:26 +0800

    chore: Update LostThingDetailScreen to use StatefulWidget and AnimationController

--- final_project_front/lib/screens/lost_thing_detail_screen.dart
+++ final_project_front/lib/screens/lost_thing_detail_screen.dart
@@ -6,17 +6,51 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:transparent_image/transparent_image.dart';
 import 'package:final_project/models/lost_thing.dart';
 
-class LostThingDetailScreen extends StatelessWidget {
-  const LostThingDetailScreen({super.key, required this.lostThings});
+class LostThingDetailScreen extends StatefulWidget {
   final LostThing lostThings;
+  const LostThingDetailScreen({super.key, required this.lostThings});
+
+  @override
+  State<LostThingDetailScreen> createState() => _LostThing();
+}
+
+class _LostThing extends State<LostThingDetailScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
 
   Future<SharedPreferences> _getPrefs() async {
     return await SharedPreferences.getInstance();
   }
 
   @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      duration: const Duration(seconds: 1),
+      vsync: this,
+    );
+
+    _animation = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.elasticOut,
+    );
+
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final LostThing lostThings = widget.lostThings;
     String authEmail = '';
+
     _getPrefs().then((prefs) {
       authEmail = prefs.getString('email')!;
     });
@@ -36,55 +70,32 @@ class LostThingDetailScreen extends StatelessWidget {
             children: [
               Card(
                 elevation: 5,
-                child: InkWell(
-                  onTap: () async {
-                    if (authEmail == lostThings.postUserEmail) {
-                      ShowAlertDialog()
-                          .showAlertDialog('無法與自己聊天', '無法與自己的帳號進行聊天', context);
-                      return;
-                    }
-                    await CreateNewChatRoom()
-                        .createNewChatRoom(lostThings.postUserEmail, authEmail)
-                        .then(
-                      (chatID) {
-                        Navigator.of(context).pushReplacement(
-                          MaterialPageRoute(
-                            builder: (ctx) => ChatScreen(
-                              chatID: chatID,
-                              chatUserEmail: lostThings.postUserEmail,
+                child: Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              lostThings.location,
+                              style: const TextStyle(fontSize: 16),
                             ),
-                          ),
-                        );
-                      },
-                    );
-                  },
-                  child: Padding(
-                    padding: const EdgeInsets.all(10),
-                    child: Row(
-                      children: [
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                lostThings.location,
-                                style: const TextStyle(fontSize: 16),
-                              ),
-                              const SizedBox(height: 5),
-                              Text(
-                                lostThings.postUser,
-                                style: const TextStyle(fontSize: 16),
-                              ),
-                            ],
-                          ),
+                            const SizedBox(height: 5),
+                            Text(
+                              lostThings.postUser,
+                              style: const TextStyle(fontSize: 16),
+                            ),
+                          ],
                         ),
-                        CircleAvatar(
-                          backgroundImage: NetworkImage(lostThings
-                              .headShotUrl), // Consider adding a user image property to the model
-                          radius: 24,
-                        ),
-                      ],
-                    ),
+                      ),
+                      CircleAvatar(
+                        backgroundImage: NetworkImage(lostThings
+                            .headShotUrl), // Consider adding a user image property to the model
+                        radius: 24,
+                      ),
+                    ],
                   ),
                 ),
               ),
@@ -108,6 +119,34 @@ class LostThingDetailScreen extends StatelessWidget {
               ),
             ],
           ),
+        ),
+      ),
+      floatingActionButton: ScaleTransition(
+        scale: _animation,
+        child: FloatingActionButton(
+          onPressed: () async {
+            if (authEmail == lostThings.postUserEmail) {
+              ShowAlertDialog()
+                  .showAlertDialog('無法與自己聊天', '無法與自己的帳號進行聊天', context);
+              return;
+            }
+            await CreateNewChatRoom()
+                .createNewChatRoom(lostThings.postUserEmail, authEmail)
+                .then(
+              (chatID) {
+                Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(
+                    builder: (ctx) => ChatScreen(
+                      chatID: chatID,
+                      chatUserEmail: lostThings.postUserEmail,
+                    ),
+                  ),
+                );
+              },
+            );
+          },
+          backgroundColor: Theme.of(context).focusColor,
+          child: const Icon(Icons.message),
         ),
       ),
     );
