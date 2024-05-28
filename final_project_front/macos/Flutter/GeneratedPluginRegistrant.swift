bfe39ab340ec79d5d7895d6ae6e01951a0453e63
box159 <aa94022728@gmail.com>
2024-05-28 15:37:12 +0800

    feat: Update dependencies for Linux and Windows platforms
    This commit updates the dependencies for the Linux and Windows platforms. It adds the `objectbox_flutter_libs` plugin to the Linux and Windows generated plugin registrant files, ensuring that the plugin is properly registered for use in the Flutter application. This update improves compatibility and functionality for the Linux and Windows versions of the application.

--- final_project_front/macos/Flutter/GeneratedPluginRegistrant.swift
+++ final_project_front/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -12,6 +12,7 @@ import firebase_core
 import firebase_storage
 import flutter_image_compress_macos
 import geolocator_apple
+import objectbox_flutter_libs
 import path_provider_foundation
 import shared_preferences_foundation
 import sqflite
@@ -25,6 +26,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
   FlutterImageCompressMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterImageCompressMacosPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  ObjectboxFlutterLibsPlugin.register(with: registry.registrar(forPlugin: "ObjectboxFlutterLibsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
